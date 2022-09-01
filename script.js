a001250e1b610878a713f49a35fc9a033d360096
atif298 <atifuddin03@gmail.com>
2022-09-01 08:04:52 +0100

    Notes added and cleaned up

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ const lights = document.querySelector(".reaction-test__lights")
 const timer = document.querySelector(".reaction-test__time")
 const scoreBoard = document.querySelector(".reaction-test__score")
 
+//Records the current time when function is called
 let startButtonTime = () => clickedStartButtonNow = new Date();
 let lightsGreenTime = () => lightGreenNow = new Date();
 let clickedLightsTime = () => clickedLightsNow = new Date();
 
-
+//Delays to a random time within 10 seconds
 let randomLightsOn = () => setTimeout (beginLights, (Math.random() * 10000));
-
 
 let recordTime = () => {
     clickedLightsTime();
     let reactionTime = document.createElement("li");
     reactionTime.innerHTML = Number(clickedLightsNow - lightGreenNow);
+    //Recording time
     scoreBoard.appendChild(reactionTime);
+    //Removing ability for multiple clicks
     lights.removeEventListener('click', recordTime);
     }
-
-
-
 
 const beginLights = () => {
     console.log(clickedStartButtonNow.getSeconds());
@@ -30,33 +29,29 @@ const beginLights = () => {
     lights.addEventListener("click", recordTime);
 }
 
-
 const start = () => {
-    console.log("start");
+    //Styling into stop button
     //#ff0000 = ${red}
     startStopBtn.style = "background-color: #ff0000";
     startStopBtn.innerHTML = "STOP"
+    //Removes start button function and add stop button function
     startStopBtn.removeEventListener("click", start);
-    startStopBtn.addEventListener("click", stop);
-    // startStopBtn.value = "stop";
-    //setTimout activates beginLights function
-    //Math.random is a random number between 0- 1
-    // * 10000to change milliseconds to seconds
+    startStopBtn.addEventListener("click", stop)
     startButtonTime();
     randomLightsOn();
 }
 
 const stop = () =>{
     clearTimeout(randomLightsOn);
+    //Styling lights to original form
     lights.style = "background-color: #7c7c7c";
-    console.log("stop");
+    //Styling into start button
     //#00c000 = ${green}
     startStopBtn.style = "background-color: #00c000";
     startStopBtn.innerHTML = "START"
+    //Removes stop button function and add start button function
     startStopBtn.removeEventListener("click", stop);
     startStopBtn.addEventListener("click", start);
-    // startStopBtn.value = "start";
-
 }
 
 startStopBtn.addEventListener("click", start);
